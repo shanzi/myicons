d51c9f85dfc6476be1d32fb0f3a2b5d21ac23a9d
chase <chase@local>
2014-11-04 20:19:49 +0800

    add login required for index

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, include, url
 
 from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = patterns(
     '',
-    url(r'^$', TemplateView.as_view(template_name='index.html')),
+    url(r'^$', login_required(TemplateView.as_view(template_name='index.html'))),
 )
